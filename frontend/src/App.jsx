1e51df96bfa3225aed8e73862ac18ff11e527407
Yash <yashbest005@gmail.com>
2024-04-04 11:22:37 -0300

    frontend react initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <div className="px-20">
+        <Button>Hello ShadCN</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
